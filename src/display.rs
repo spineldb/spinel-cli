d0a2bac0baa41a25b986d66449199bebc168dfee
Riki Permana <arwahdevops@gmail.com>
2025-11-14 06:58:49 +0700

    fix: format geospatial

--- src/display.rs
+++ src/display.rs
@@ -1,28 +1,41 @@
 use spineldb::core::protocol::RespFrame;
 use std::io;
 
-pub fn print_resp_frame(frame: &RespFrame, writer: &mut dyn io::Write) -> io::Result<()> {
+fn _print_resp_frame(
+    frame: &RespFrame,
+    writer: &mut dyn io::Write,
+    level: usize,
+) -> io::Result<()> {
+    let indent = "   ".repeat(level);
     match frame {
-        RespFrame::SimpleString(s) => writeln!(writer, "{}", s),
-        RespFrame::Error(e) => writeln!(writer, "(error) {}", e),
-        RespFrame::Integer(i) => writeln!(writer, "(integer) {}", i),
+        RespFrame::SimpleString(s) => write!(writer, "{}", s),
+        RespFrame::Error(e) => write!(writer, "(error) {}", e),
+        RespFrame::Integer(i) => write!(writer, "(integer) {}", i),
         RespFrame::BulkString(bytes) => {
             if let Ok(s) = String::from_utf8(bytes.to_vec()) {
-                writeln!(writer, "\"{}\"", s)
+                write!(writer, "\"{}\"", s)
             } else {
-                writeln!(writer, "{:?}", bytes)
+                write!(writer, "{:?}", bytes)
             }
         }
-        RespFrame::Null => writeln!(writer, "(nil)"),
-        RespFrame::NullArray => writeln!(writer, "(empty array)"),
+        RespFrame::Null => write!(writer, "(nil)"),
+        RespFrame::NullArray => write!(writer, "(empty array)"),
         RespFrame::Array(frames) => {
             for (i, frame) in frames.iter().enumerate() {
-                write!(writer, "{}. ", i + 1)?;
-                print_resp_frame(frame, writer)?;
+                if i > 0 {
+                    write!(writer, "\n{}", indent)?;
+                }
+                write!(writer, "{}) ", i + 1)?;
+                _print_resp_frame(frame, writer, level + 1)?;
             }
             Ok(())
         }
     }
+}
+
+pub fn print_resp_frame(frame: &RespFrame, writer: &mut dyn io::Write) -> io::Result<()> {
+    _print_resp_frame(frame, writer, 0)?;
+    writeln!(writer)
 }
 
 pub fn print_resp_frame_to_stdout(frame: &RespFrame) {
